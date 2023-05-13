28d83cce58ad818504e14466c9c0341e1e7c87fc
Fitra R <39044004+Rizki36@users.noreply.github.com>
2023-05-13 21:57:01 +0700

    feat: add endpoint patch provider detail

--- src/controllers/users/users.controller.js
+++ src/controllers/users/users.controller.js
@@ -72,3 +72,48 @@ export const getUsers = async (req, res, next) => {
     next(error);
   }
 };
+
+export const patchProviderDetail = async (req, res, next) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: req.email,
+      },
+    });
+
+    if (!user)
+      return res.status(404).json({
+        error: true,
+        message: "User not found",
+        data: [],
+      });
+
+    const response = await prisma.providerDetail.update({
+      where: {
+        userId: user.id,
+      },
+      data: {
+        ...req.body,
+      },
+      include: {
+        user: true,
+      },
+    });
+
+    if (!response)
+      return res.status(400).json({
+        error: true,
+        message: "Failed update data",
+        data: [],
+      });
+
+    return res.status(200).json({
+      error: false,
+      message: "Success update data",
+      data: response,
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
--- src/routes/users/users.routes.js
+++ src/routes/users/users.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import * as users from "../../controllers/users/users.controller.js";
+import { verifyToken } from "../../middlewares/auth/auth.middleware.js";
 
 const router = Router();
 
 router.get("/", users.getUsers);
+router.patch("/provider-detail", verifyToken, users.patchProviderDetail);
 
 export default router;
